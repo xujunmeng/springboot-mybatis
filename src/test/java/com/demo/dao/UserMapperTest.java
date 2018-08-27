d868d9e8f4772803d91d6c8a135d4fdb29b943a7
xujunmeng.github <xujunmeng2012@163.com>
2018-08-27 17:34:11 +0800

    init

---
+++ src/test/java/com/demo/dao/UserMapperTest.java
@@ -0,0 +1,27 @@
+package com.demo.dao;
+
+import com.demo.entity.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by james on 2018/8/27.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+public class UserMapperTest {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Test
+    public void testSelectByPrimaryKey() throws Exception {
+        User user = userMapper.selectByPrimaryKey(1);
+        System.out.println(user);
+    }
+}
