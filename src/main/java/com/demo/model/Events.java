06a80b5e14f8ce59dbf5e4687f1b96ae0d9e82b4
xujunmeng.github <xujunmeng2012@163.com>
2018-09-17 16:33:26 +0800

    spring boot 状态机制

---
+++ src/main/java/com/demo/model/Events.java
@@ -0,0 +1,14 @@
+package com.demo.model;
+
+/**
+ *
+ * 引起状态迁移的事件
+ *
+ * Created by james on 2018/9/17.
+ */
+public enum Events {
+
+    支付,
+    收货
+
+}
---
+++ src/main/java/com/demo/model/States.java
@@ -0,0 +1,15 @@
+package com.demo.model;
+
+/**
+ *
+ * 订单状态
+ *
+ * Created by james on 2018/9/17.
+ */
+public enum States {
+
+    待支付,
+    待收货,
+    结束
+
+}
